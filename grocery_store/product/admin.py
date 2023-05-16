e320d2fb8146233363e515e90a57221c9aa04aa8
vasya <vasya1010101010@gmail.com>
2023-05-16 20:38:04 +0300

    add

--- grocery_store/product/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Category, Card, Counterparty, Product
-
-# Register your models here.
-admin.site.register(Category)
-admin.site.register(Card)
-admin.site.register(Counterparty)
-admin.site.register(Product)
